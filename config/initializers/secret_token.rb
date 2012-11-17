e2d26c29afb1cd1db87d98c22d24afb0a43b23a5
Omar Vargas <ovargas27@gmail.com>
2012-11-17 13:35:06 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PocketizeHmodevhouse::Application.config.secret_token = '65eff5cd6895fdbbe5e270821ebe7e2190e693d04e681fac142707976aa210047fc3e170cd8229f94baf77868e42109bd1dd163156c9fe4ce347773044be9404'
